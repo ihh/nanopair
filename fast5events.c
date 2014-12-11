b542c9fddbbc28559943fa95af16af09d933b25d
Ian Holmes <github@yam.biowiki.org>
2014-12-11 15:44:55 -0800

    getting there (still trying to read f'in data from HDF5)

--- fast5events.c
+++ fast5events.c
@@ -63,46 +63,31 @@ void free_fast5_event_array (fast5_event_array* ev);
 typedef struct fast5_event_array_iterator {
   fast5_event_array* event_array;
   int event_array_index, n_template_events;
-  size_t strand_offset, mean_offset, stdv_offset, length_offset, model_state_offset, move_offset, mp_model_state_offset, raw_offset;
+  size_t mean_offset, stdv_offset, length_offset, model_state_offset, move_offset, mp_model_state_offset, raw_offset;
   int model_order;
 } fast5_event_array_iterator;
 
-herr_t count_template_events (void *elem, hid_t type_id, unsigned ndim, 
-			      const hsize_t *point, void *operator_data)
-{
-  fast5_event_array_iterator *iter = (fast5_event_array_iterator*) operator_data;
-  char* strand = *((char**) (elem + iter->strand_offset));
-  if (strcmp(strand,"template") == 0)
-    {
-      ++iter->n_template_events;
-      return 0;
-    }
-  return 1;
-}
+/*
 
 herr_t populate_event_array (void *elem, hid_t type_id, unsigned ndim, 
 			     const hsize_t *point, void *operator_data)
 {
   fast5_event_array_iterator *iter = (fast5_event_array_iterator*) operator_data;
-  char* strand = *((char**) (elem + iter->strand_offset));
-  if (strcmp(strand,"template") == 0)
-    {
-      fast5_event* ev = iter->event_array->event + (iter->event_array_index++);
-      ev->mean = (double) *((H5T_IEEE_F64LE*) (elem + iter->mean_offset));
-      ev->stdv = (double) *((H5T_IEEE_F64LE*) (elem + iter->stdv_offset));
-      ev->length = (double) *((H5T_IEEE_F64LE*) (elem + iter->length_offset));
-      for (int n = 0; n < iter->model_order; ++n) {
-	ev->model_state[n] = *((char*) (elem + iter->model_state_offset + n));
-	ev->mp_model_state[n] = *((char*) (elem + iter->mp_model_state_offset + n));
-      }
-      ev->model_state[iter->model_order] = ev->mp_model_state[iter->model_order] = '\0';
-      ev->move = (int) *((H5T_STD_I64LE*) (elem + iter->move_offset));
-      ev->raw = (int) *((H5T_STD_I64LE*) (elem + iter->raw_offset));
-      return 0;
-    }
-  return 1;
-}
 
+  fast5_event* ev = iter->event_array->event + (iter->event_array_index++);
+  ev->mean = (double) *((H5T_IEEE_F64LE*) (elem + iter->mean_offset));
+  ev->stdv = (double) *((H5T_IEEE_F64LE*) (elem + iter->stdv_offset));
+  ev->length = (double) *((H5T_IEEE_F64LE*) (elem + iter->length_offset));
+  for (int n = 0; n < iter->model_order; ++n) {
+  ev->model_state[n] = *((char*) (elem + iter->model_state_offset + n));
+  ev->mp_model_state[n] = *((char*) (elem + iter->mp_model_state_offset + n));
+  }
+  ev->model_state[iter->model_order] = ev->mp_model_state[iter->model_order] = '\0';
+  ev->move = (int) *((H5T_STD_I64LE*) (elem + iter->move_offset));
+  ev->raw = (int) *((H5T_STD_I64LE*) (elem + iter->raw_offset));
+  return 0;
+}
+*/
 
 
 
@@ -187,25 +172,17 @@ int main(int argc, char * argv[])
 {
 	/* file name */
 	char * fn = NULL;
-	/* set of hdf paths we check for events data */
-        char const * events_paths[] =
-        {
-	        "/Analyses/Basecall_2D_000/BaseCalled_2D/Events",
-	        "/Analyses/Basecall_2D_000/BaseCalled_template/Events",
-	        "/Analyses/Basecall_2D_000/BaseCalled_complement/Events",
-	        0
-        };
-	/* iterator for events_paths */
-	char const ** evp = NULL;
-	/* still searching for events */
-        int running = 1;
+	/* path we check for events data */
+	const char* path = "/Analyses/Basecall_2D_000/BaseCalled_template/Events";
         /* file handle */
         hid_t file_id;
         /* op status */
         herr_t status;
 
 	/* event_array */
+	/*
 	fast5_event_array* event_array = NULL;
+	*/
 
 	/* check for input file name */
 	if ( !(1<argc) )
@@ -237,84 +214,96 @@ int main(int argc, char * argv[])
 	    return EXIT_FAILURE;
 	  }
 
-        /* try list of paths to events data */
-        for ( evp = &events_paths[0]; running && *evp; ++evp )
-        {
-        	/* events path */
-        	char const * path = *evp;
-		
-		/* see if path exists */
-		if (H5LTpath_valid ( file_id, path, 1))
-		  {
-		    fprintf(stderr,"path %s is valid\n",path);
+	/* see if path exists */
+	if (H5LTpath_valid ( file_id, path, 1))
+	  {
+	    fprintf(stderr,"path %s is valid\n",path);
 
-		    /* get dataset */
-		    hid_t events_id = H5Oopen(file_id, path, H5P_DEFAULT);
-		    if ( events_id < 0 )
-		      {
-			fprintf(stderr,"failed to open dataset %s\n",path);
-			break;
-		      }
+	    /* get dataset */
+	    hid_t events_id = H5Oopen(file_id, path, H5P_DEFAULT);
+	    if ( events_id < 0 )
+	      {
+		fprintf(stderr,"failed to open dataset %s\n",path);
+	      }
+	    else
+	      {
 
-		    hid_t events_type_id = H5Dget_type( events_id );
+		/* get information about fields in an event */
+		hid_t events_type_id = H5Dget_type( events_id );
 
-		    fast5_event_array_iterator iter;
+		fast5_event_array_iterator iter;
 
-		    int strand_idx = H5Tget_member_index( events_type_id, "strand" );
-		    int mean_idx = H5Tget_member_index( events_type_id, "mean" );
-		    int stdv_idx = H5Tget_member_index( events_type_id, "stdv" );
-		    int length_idx = H5Tget_member_index( events_type_id, "length" );
-		    int model_state_idx = H5Tget_member_index( events_type_id, "model_state" );
-		    int move_idx = H5Tget_member_index( events_type_id, "move" );
-		    int mp_model_state_idx = H5Tget_member_index( events_type_id, "mp_model_state" );
-		    int raw_idx = H5Tget_member_index( events_type_id, "raw_index" );
+		int mean_idx = H5Tget_member_index( events_type_id, "mean" );
+		int stdv_idx = H5Tget_member_index( events_type_id, "stdv" );
+		int length_idx = H5Tget_member_index( events_type_id, "length" );
+		int model_state_idx = H5Tget_member_index( events_type_id, "model_state" );
+		int move_idx = H5Tget_member_index( events_type_id, "move" );
+		int mp_model_state_idx = H5Tget_member_index( events_type_id, "mp_state" );
+		int raw_idx = H5Tget_member_index( events_type_id, "raw_index" );
 
-		    iter.strand_offset = H5Tget_member_offset( events_type_id, strand_idx );
-		    iter.mean_offset = H5Tget_member_offset( events_type_id, mean_idx );
-		    iter.stdv_offset = H5Tget_member_offset( events_type_id, stdv_idx );
-		    iter.length_offset = H5Tget_member_offset( events_type_id, length_idx );
-		    iter.model_state_offset = H5Tget_member_offset( events_type_id, model_state_idx );
-		    iter.move_offset = H5Tget_member_offset( events_type_id, move_idx );
-		    iter.mp_model_state_offset = H5Tget_member_offset( events_type_id, mp_model_state_idx );
-		    iter.raw_offset = H5Tget_member_offset( events_type_id, raw_idx );
+		fprintf(stderr,"member indices: %d %d %d %d %d %d %d\n", mean_idx, stdv_idx, length_idx, model_state_idx, move_idx, mp_model_state_idx, raw_idx);
 
-		    iter.model_order =  (int) H5Tget_size (H5Tget_member_type( events_type_id, model_state_idx ));
+		iter.mean_offset = H5Tget_member_offset( events_type_id, mean_idx );
+		iter.stdv_offset = H5Tget_member_offset( events_type_id, stdv_idx );
+		iter.length_offset = H5Tget_member_offset( events_type_id, length_idx );
+		iter.model_state_offset = H5Tget_member_offset( events_type_id, model_state_idx );
+		iter.move_offset = H5Tget_member_offset( events_type_id, move_idx );
+		iter.mp_model_state_offset = H5Tget_member_offset( events_type_id, mp_model_state_idx );
+		iter.raw_offset = H5Tget_member_offset( events_type_id, raw_idx );
 
-		    fprintf(stderr,"model_order is %d\n",iter.model_order);
+		fprintf(stderr,"member offsets: %lu %lu %lu %lu %lu %lu %lu\n", iter.mean_offset, iter.stdv_offset, iter.length_offset, iter.model_state_offset, iter.move_offset, iter.mp_model_state_offset, iter.raw_offset);
 
-		    hid_t events_space_id = H5Dget_space( events_id );
-		    hssize_t events_npoints = H5Sget_simple_extent_npoints( events_space_id );
+		iter.model_order =  (int) H5Tget_size (H5Tget_member_type( events_type_id, model_state_idx ));
 
-		    fprintf(stderr,"dataset has %lld points\n",events_npoints);
+		fprintf(stderr,"model_order is %d\n",iter.model_order);
 
-		    /* read into memory buffer */
-		    hsize_t buf_size = H5Dget_storage_size( events_id );
-		    void *buf = SafeMalloc (buf_size);
-		    H5Dread( events_id, events_type_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf );
 
-		    /* iterate to find number of template strand events */
-		    iter.n_template_events = 0;
-		    H5Diterate( buf, events_type_id, events_id, count_template_events, &iter );
+		/* get dimensions */
+		hid_t events_space_id = H5Dget_space( events_id );
+		hssize_t events_npoints = H5Sget_simple_extent_npoints( events_space_id );
+		size_t event_size = H5Tget_size( events_type_id );
 
-		    /* allocate & populate event_array */
-		    event_array = iter.event_array = alloc_fast5_event_array (iter.model_order, iter.n_template_events);
-		    iter.event_array_index = 0;
-		    H5Diterate( buf, events_type_id, events_id, populate_event_array, &iter );
+		fprintf(stderr,"dataset has %lld points, event takes %lu bytes\n",events_npoints,event_size);
 
-		    /* free */
-		    SafeFree (buf);
+
+		/*
+		 * Create variable-length string datatype.
+		 */
+		hid_t strtype = H5Tcopy (H5T_C_S1);
+		H5Tset_size (strtype, H5T_VARIABLE);
+		    
+		/*
+		 * Create the compound datatype for memory.
+		 */
+		hid_t fast5_event_type_id = H5Tcreate (H5T_COMPOUND, sizeof (fast5_event));
+		H5Tinsert (fast5_event_type_id, "mean", HOFFSET (fast5_event, mean), H5T_NATIVE_DOUBLE);
+		H5Tinsert (fast5_event_type_id, "stdv", HOFFSET (fast5_event, stdv), H5T_NATIVE_DOUBLE);
+		H5Tinsert (fast5_event_type_id, "length", HOFFSET (fast5_event, length), H5T_NATIVE_DOUBLE);
+		H5Tinsert (fast5_event_type_id, "model_state", HOFFSET (fast5_event, model_state), strtype);
+		H5Tinsert (fast5_event_type_id, "mp_model_state", HOFFSET (fast5_event, mp_model_state), strtype);
+		H5Tinsert (fast5_event_type_id, "move", HOFFSET (fast5_event, move), H5T_NATIVE_INT);
+		H5Tinsert (fast5_event_type_id, "raw_index", HOFFSET (fast5_event, raw), H5T_NATIVE_INT);
+
+		/* read into memory buffer */
+		void *buf = SafeMalloc (events_npoints * event_size);
+		H5Dread( events_id, events_type_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, buf );
+
+		/* convert */
+		/*
+		  H5Tconvert( events_type_id, fast5_event_type_id, events_npoints, buf, NULL, H5P_DEFAULT );
+
+		  fast5_event *events = SafeMalloc (events_npoints * sizeof(fast5_event));
+		*/
+
 		      
-		    /* close */
-		    H5Sclose(events_space_id);
-		    H5Tclose(events_type_id);
-		    H5Dclose(events_id);
-
-		    /* stop looking */
-		    running = 0;
-		  }
-		else
-		  fprintf(stderr,"path %s not valid\n",path);
-	}
+		/* close */
+		H5Sclose(events_space_id);
+		H5Tclose(events_type_id);
+		H5Dclose(events_id);
+	      }
+	  }
+	else
+	  fprintf(stderr,"path %s not valid\n",path);
 
 	/* close root */
 	H5Gclose(root_id);
