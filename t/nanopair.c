10be9ee6e3a8fe0b3e99fb1a1611ed3a0b7d22d2
Ian Holmes <github@yam.biowiki.org>
2015-01-20 16:22:30 -0800

    t/nanopair.c

---
+++ t/nanopair.c
@@ -0,0 +1,28 @@
+#include <locale.h>
+
+#include "../src/seqevtpair.h"
+
+void print_help() {
+    printf ("Usage:\n"
+	    "nanopair train <FASTA file> <Fast5 file> [<more Fast5 files...>]   > paramfile.xml\n"
+	    "nanopair align <paramfile> <FASTA file> <Fast5file> [<more fast5 files...>]  > hits.gff\n");
+}
+
+Vector* init_fast5_event_array_vector (int argc, char** argv) {
+  /* MORE TO GO HERE */
+  return NULL;
+}
+
+int main (int argc, char** argv) {
+  if (argc <= 1)
+    print_help();
+  else if (strcmp (argv[1], "train") == 0) {
+    /* MORE TO GO HERE */
+  } else if (strcmp (argv[1], "align") == 0) {
+    /* MORE TO GO HERE */
+  } else {
+    printf ("Unrecognized command\n\n");
+    print_help();
+  }
+  return EXIT_SUCCESS;
+}
