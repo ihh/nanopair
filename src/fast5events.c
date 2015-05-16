9d8a2e842d01bf2a498f0151b774dccf3f3faa69
Ian Holmes <github@yam.biowiki.org>
2015-05-16 06:53:07 +0100

    start field in fast5

--- src/fast5events.c
+++ src/fast5events.c
@@ -11,6 +11,7 @@ const char* events_path = "/Analyses/Basecall_2D_000/BaseCalled_template/Events"
 
 /* names of various member fields in HDF5 file */
 #define FAST5_EVENT_MEAN "mean"
+#define FAST5_EVENT_START "start"
 #define FAST5_EVENT_STDV "stdv"
 #define FAST5_EVENT_LENGTH "length"
 #define FAST5_EVENT_MODEL_STATE "model_state"
@@ -23,7 +24,7 @@ const char* events_path = "/Analyses/Basecall_2D_000/BaseCalled_template/Events"
 typedef struct Fast5_event_array_iterator {
   Fast5_event_array* event_array;
   int event_array_index;
-  size_t mean_offset, stdv_offset, length_offset, model_state_offset, move_offset, mp_model_state_offset, raw_offset;
+  size_t mean_offset, start_offset, stdv_offset, length_offset, model_state_offset, move_offset, mp_model_state_offset, raw_offset;
   int model_order;
 } Fast5_event_array_iterator;
 
@@ -34,6 +35,7 @@ herr_t populate_event_array (void *elem, hid_t type_id, unsigned ndim,
 
   Fast5_event* ev = iter->event_array->event + (iter->event_array_index++);
   ev->mean = *((double*) (elem + iter->mean_offset));
+  ev->start = *((double*) (elem + iter->start_offset));
   ev->stdv = *((double*) (elem + iter->stdv_offset));
   ev->length = *((double*) (elem + iter->length_offset));
 
@@ -148,6 +150,7 @@ Fast5_event_array* read_fast5_event_array (const char* filename, double tick_len
 	  Fast5_event_array_iterator iter;
 
 	  int mean_idx = H5Tget_member_index( events_type_id, FAST5_EVENT_MEAN );
+	  int start_idx = H5Tget_member_index( events_type_id, FAST5_EVENT_START );
 	  int stdv_idx = H5Tget_member_index( events_type_id, FAST5_EVENT_STDV );
 	  int length_idx = H5Tget_member_index( events_type_id, FAST5_EVENT_LENGTH );
 	  int model_state_idx = H5Tget_member_index( events_type_id, FAST5_EVENT_MODEL_STATE );
@@ -156,6 +159,7 @@ Fast5_event_array* read_fast5_event_array (const char* filename, double tick_len
 	  int raw_idx = H5Tget_member_index( events_type_id, FAST5_EVENT_RAW_INDEX );
 
 	  iter.mean_offset = H5Tget_member_offset( events_type_id, mean_idx );
+	  iter.start_offset = H5Tget_member_offset( events_type_id, start_idx );
 	  iter.stdv_offset = H5Tget_member_offset( events_type_id, stdv_idx );
 	  iter.length_offset = H5Tget_member_offset( events_type_id, length_idx );
 	  iter.model_state_offset = H5Tget_member_offset( events_type_id, model_state_idx );
